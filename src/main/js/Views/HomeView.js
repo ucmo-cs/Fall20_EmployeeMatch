5e50efc95ed4f1f6d826fc420379d8283099e3d8
Nehemiah Deason <nijanemi@yahoo.com>
2020-10-25 20:29:21 -0500

    setting up background image for home page

--- src/main/js/Views/HomeView.js
+++ src/main/js/Views/HomeView.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import HandShake from '../../resources/Images/HandShake.jpg'
 
 function HomeView()
 {
@@ -7,7 +8,9 @@ function HomeView()
         <div>
             <h1>This is the home page</h1>
             <div className={"p-3"}></div>
-            <div></div>
+            <div  className="homePageBackground">
+                <h1>Hello With Image</h1>
+            </div>
         </div>
     );
 }

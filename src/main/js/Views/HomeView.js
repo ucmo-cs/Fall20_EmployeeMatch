1ab15213dc7313e909bb9f0c15e5d0e156f0d0ca
Nehemiah Deason <nijanemi@yahoo.com>
2020-10-27 20:16:02 -0500

    Completed Home Screen

--- src/main/js/Views/HomeView.js
+++ src/main/js/Views/HomeView.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import HandShake from '../../resources/Images/HandShake.jpg'
+//import HandShake from '../../resources/Images/HandShake.jpg'
 
 function HomeView()
 {
 
+
     return (
         <div>
-            <h1>This is the home page</h1>
-            <div className={"p-3"}></div>
-            <div  className="homePageBackground">
-                <h1>Hello With Image</h1>
+            <div className="mainPage row" >
+                <h1 className="text-center col-12 align-self-center">Partnering with employees and employers to match them according to
+                    their preferences since 2020</h1>
             </div>
         </div>
     );
 }
+//<img src="Images/HandShake.jpg"/>
+//
 export default HomeView
